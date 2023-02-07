d8cd8392f3efe1812e2f1442a2e3de347c59030f
keerthi kantheti <kvkeerthi2001@gmail.com>
2023-02-07 19:54:03 +0000

    React Jsx code for super league page

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="super-league-bg-container">
+    <h1 className="super-league-heading">Super Over League</h1>
+    <div className="images-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="super-league-image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="super-league-image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
